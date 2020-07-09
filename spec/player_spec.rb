d4dc1f3a5e7ebee0a636a10dc551d37d4c276ea3
Emas <emas.dev@gmail.com>
2020-07-09 10:31:59 -0500

    add context to describe rspec tests and ensure all methods

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,30 +2,42 @@ require './lib/player'
 require './lib/board'
 
 describe Player do
-  it 'switch player 1' do
-    player = Player.new
-    player.switch_player
-    expect(player.player).to eq 1
-    expect(player.symbol).to eq 'X'
+  context '#initialize' do
+    it 'init player' do
+      player = Player.new
+      expect(player.player).to eq 2
+      expect(player.symbol).to eq 'X'
+    end
   end
 
-  it 'switch the player 2' do
-    player = Player.new
-    player.switch_player
-    player.switch_player
-    expect(player.player).to eq 2
-    expect(player.symbol).to eq '●'
+  context '#switch_player' do
+    it 'switch player 1' do
+      player = Player.new
+      player.switch_player
+      expect(player.player).to eq 1
+      expect(player.symbol).to eq 'X'
+    end
+
+    it 'switch the player 2' do
+      player = Player.new
+      player.switch_player
+      player.switch_player
+      expect(player.player).to eq 2
+      expect(player.symbol).to eq '●'
+    end
   end
 
-  it 'player makes a valid move' do
-    player = Player.new
-    board = Board.new
-    expect(player.move(1, board)).to eq true
-  end
+  context '#move' do
+    it 'player makes a valid move' do
+      player = Player.new
+      board = Board.new
+      expect(player.move(1, board)).to eq true
+    end
 
-  it 'player makes a invalid move' do
-    player = Player.new
-    board = Board.new
-    expect(player.move(10, board)).to eq false
+    it 'player makes a invalid move' do
+      player = Player.new
+      board = Board.new
+      expect(player.move(10, board)).to eq false
+    end
   end
 end
--- spec/scores_spec.rb
+++ spec/scores_spec.rb
@@ -1,21 +1,23 @@
 require './lib/scores'
 
 describe Scores do
-  it 'sums draw plus 1' do
-    score = Scores.new
-    score.score(1, 'win')
-    expect(score.player1).to eq 1
-  end
+  context '#score' do
+    it 'add 1 to player 1 score' do
+      score = Scores.new
+      score.score(1, 'win')
+      expect(score.player1).to eq 1
+    end
 
-  it 'sums draw plus 1' do
-    score = Scores.new
-    score.score(2, 'win')
-    expect(score.player2).to eq 1
-  end
+    it 'add 1 to player 2 score' do
+      score = Scores.new
+      score.score(2, 'win')
+      expect(score.player2).to eq 1
+    end
 
-  it 'sums draw plus 1' do
-    score = Scores.new
-    score.score(1, '')
-    expect(score.draw).to eq 1
+    it 'add 1 to number of draws matches' do
+      score = Scores.new
+      score.score(1, '')
+      expect(score.draw).to eq 1
+    end
   end
 end
